c2b63a5eb15ede5d2ed2924d1c7a944df68c5e92
Arc <atandarokib@gmail.com>
2023-07-04 00:59:13 +0100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
 import Head from "./Components/Head";
 import Footer from "./Components/Footer";
 
 const App = () => {
+  useEffect(() => {
+    document.querySelectorAll("img").forEach((image) => {
+      image.setAttribute("loading", "lazy");
+    });
+  });
   return (
     <div className="min-h-screen w-full relative font-serif bg-neutral-200 text-black parent">
       <Header />
